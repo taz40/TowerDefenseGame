7df1b142ddd01bb09ccaf8e6163e894fa77e33e2
PiWalker <ldspcjunkie@gmail.com>
2015-10-13 20:58:00 -0600

    started working on the key handler, and exit feature.

---
+++ src/com/weebly/lightsoutg4ming/towerdefense/KeyHandler.java
@@ -0,0 +1,28 @@
+package com.weebly.lightsoutg4ming.towerdefense;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
+			System.exit(0);
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
--- src/com/weebly/lightsoutg4ming/towerdefense/Screen.java
+++ src/com/weebly/lightsoutg4ming/towerdefense/Screen.java
@@ -1,18 +1,45 @@
 package com.weebly.lightsoutg4ming.towerdefense;
 
+import java.awt.Graphics;
+
 import javax.swing.JPanel;
 
 public class Screen extends JPanel implements Runnable{
 	
 	Thread thread = new Thread(this);
+	Frame frame;
+	
+	private int fps = 0;
 	
 	public Screen(Frame frame){
+		this.frame = frame;
 		thread.start();
+		this.frame.addKeyListener(new KeyHandler());
+	}
+	
+	public void paintComponent(Graphics g){
+		g.clearRect(0, 0, frame.getWidth(), frame.getHeight());
+		
+		g.drawString(fps+" fps", 10, 20);
 	}
 
 	public void run() {
+		long lastFrame = System.currentTimeMillis();
+		int frames = 0;
 		
-		
+		while(true){
+			repaint();
+			
+			frames++;
+			if(System.currentTimeMillis() - 1000 >= lastFrame){
+				fps = frames;
+				frames = 0;
+				lastFrame = System.currentTimeMillis();
+			}
+			try {
+				Thread.sleep(2);
+			} catch (InterruptedException e) {}
+		}
 		
 	}
 	
